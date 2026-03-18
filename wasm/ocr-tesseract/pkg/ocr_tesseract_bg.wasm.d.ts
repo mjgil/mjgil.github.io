72a0c5c24f1016c505b37e24ff443197888ac4a3
Malcom Gilbert <malcomgilbert@gmail.com>
2026-03-18 02:54:24 -0400

    add ocr tesseract

---
+++ wasm/ocr-tesseract/pkg/ocr_tesseract_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: () => void;
+export const wasm_bindgen__closure__destroy__h9a5809a146a816b5: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h6a0423b3ba63a22f: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h6a0423b3ba63a22f_1: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h6a0423b3ba63a22f_2: (a: number, b: number, c: any) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
